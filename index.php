1aebc97c74c5f6ef2a7112196255c0d3b9d1716a
lamadoraS <lamadoraa2@gmail.com>
2023-06-03 11:53:47 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include 'database.php';
+
+class Department extends Database 
+{
+    public $tblName="employee";
+
+    public function table()
+    {
+    
+        $create="CREATE TABLE IF NOT EXISTS $this->tblName (
+            id int primary key auto_increment,
+            first_name varchar(200),
+            last_name varchar(200)
+            )"; 
+        $this->conn->query($create);          
+}
+public function insert()
+{
+   $i= "INSERT INTO $this->tblName 
+      VALUES (1,'antonette','lozares')";
+      $this->conn->query($i);
+      var_dump($this->conn->error);
+
+}
+    
+} 
+$e= new Department();
+$e->table();
+$e->insert();
